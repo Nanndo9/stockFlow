d20409d29b7c6f779128aba93e7e11b7cfa5116e
Nanndo9 <gofernando44@gmail.com>
2025-07-11 19:45:38 -0300

    feat: implement service methods for product operations
    - Add Create_Product_service with Duplicate Sku Validation
    - Implemes get_all_product to list and convert products
    - Creates get_by_id with proper product treatment not found
    - Uses mapping between entities and DTOS with model_validate

--- app/services/productsService.py
+++ app/services/productsService.py
@@ -1,3 +1,6 @@
+import uuid
+from fastapi import HTTPException, status
+from app.exceptions.product import ProductNotFound
 from app.repositories.productRepository import ProductRepository
 from app.schemas.productSchema import ProductCreate, ProductRead
 
@@ -7,10 +10,31 @@ class ProductService:
         self.product_repo = product_repo
 
     def create_product_service(self, product: ProductCreate) -> ProductRead:
-        db_product = self.product_repo.create(product)
+
+        product_exists = self.product_repo.get_by_sku(product.sku)
         
+        if product_exists:
+            
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"Product with Sku '{product.sku}'  already exists.",
+            )
+
+        db_product = self.product_repo.create(product)
+        self.product_repo.db.commit()
+
         return ProductRead.model_validate(db_product)
 
-    def getByProductId(self, product: ProductCreate) -> ProductRead:
-        db_product = self.product_repo.getByProductId(product)
-        return ProductRead.model_validate(db_product)
+    def get_all_product(self) -> list[ProductRead]:
+        db_product_list = self.product_repo.get_all_product()
+
+        return [ProductRead.model_validate(product) for product in db_product_list]
+
+    def get_by_id(self, product_id: uuid.UUID) -> ProductRead:
+        
+        db_product_by_id = self.product_repo.get_by_id(product_id)
+        
+        if not db_product_by_id:
+            raise ProductNotFound(product_id=product_id)
+        
+        return ProductRead.model_validate(db_product_by_id)
