a9b3623cca8736b5425c9edde260c5a6515c2932
Nanndo9 <gofernando44@gmail.com>
2025-07-11 19:40:52 -0300

    feat:create get_by_id method in productRepository feat:create get_all_products method in productRepository feat:create get_by_sku method in productRepository fix: Removes the use of Commit in the Repositories layer

--- app/repositories/productRepository.py
+++ app/repositories/productRepository.py
@@ -13,12 +13,18 @@ class ProductRepository:
     def create(self, product: ProductBase) -> Products:
         db_product = Products(**product.model_dump())
         self.db.add(db_product)
-        self.db.commit()
         self.db.refresh(db_product)
         return db_product
 
-    def getByProductId(self, product: ProductBase) -> Products:
-        db_product = Products(**product.model_dump())
-        self.db.query(Products).all()
-       
-        return db_product
+    def get_all_product(self) -> list[Products]:
+        result_all_products = self.db.query(Products).all()
+
+        return result_all_products
+
+    def get_by_id(self, id: uuid.UUID) -> Optional[Products]:
+        get_product_id = self.db.query(Products).get(id)
+        return get_product_id
+
+    def get_by_sku(self, sku: str) -> Optional[Products]:
+        get_product_sku = self.db.query(Products).filter(Products.sku == sku).first()
+        return get_product_sku
