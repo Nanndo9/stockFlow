c1f90ddcc0d2a8b74dc893a4f7c2fba065cfa600
Nanndo9 <gofernando44@gmail.com>
2025-07-11 19:35:14 -0300

    Feat: Add Centralized Exception Treatment
    - Importing register_exception_handlers from the app.handlers.register
    - Registering the exception handlers in the FASTAPI application
    - Improves error treatment throughout the API

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from app.core.dataBase import Base, engine
 from app.routes.productRoute import router as product_router
+from app.handlers.register import register_exception_handlers
+
 
 app = FastAPI(
     title="Stock Flow API", description="API para controle de estoque", version="1.0.0"
 )
+
+register_exception_handlers(app)
 
 
 Base.metadata.create_all(bind=engine)
