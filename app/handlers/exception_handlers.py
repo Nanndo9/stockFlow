563d79d6d4da0b15387c147af8ef0717ece58a59
Nanndo <gofernando44@gmail.com>
2025-07-13 16:17:40 -0300

    feat: Add exception manipulators to useless and existing users

--- app/handlers/exception_handlers.py
+++ app/handlers/exception_handlers.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException, Request, status
 from fastapi.responses import JSONResponse
 
 from app.exceptions.product import ProductNotFound, ProductNotUpdated
+from app.exceptions.users import UserAlreadyExists, UserNotFound
 
 
 async def http_exception_handler(request: Request, exc: HTTPException):
@@ -39,6 +40,34 @@ async def product_not_updated_handler(request: Request, exc: ProductNotUpdated):
             "error": {
                 "status_code": status.HTTP_400_BAD_REQUEST,
                 "detail": f"Could not update product with ID '{exc.product_id}': {exc.reason}",
+                "path": str(request.url.path),
+                "method": request.method,
+            }
+        },
+    )
+
+
+async def user_not_found_handler(request: Request, exc: UserNotFound):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={
+            "error": {
+                "status_code": status.HTTP_404_NOT_FOUND,
+                "detail": f"User with ID '{exc.user_id}' not found.",
+                "path": str(request.url.path),
+                "method": request.method,
+            }
+        },
+    )
+
+
+async def user_already_exists_handler(request: Request, exc: UserAlreadyExists):
+    return JSONResponse(
+        status_code=status.HTTP_409_CONFLICT,
+        content={
+            "error": {
+                "status_code": status.HTTP_409_CONFLICT,
+                "detail": f"User with email '{exc.user_email}' already exists.",
                 "path": str(request.url.path),
                 "method": request.method,
             }
