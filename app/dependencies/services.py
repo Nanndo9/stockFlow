61bfcbbf31c814b7d48c78eece51a3e2315d7825
Nanndo <gofernando44@gmail.com>
2025-07-13 16:21:11 -0300

    feat: adds user_service to the dependency manager

--- app/dependencies/services.py
+++ app/dependencies/services.py
@@ -1,10 +1,19 @@
 from app.dependencies.get_db import get_db
+from app.repositories.userRepository import UserRepository
 from app.services.productsService import ProductService
 from app.repositories.productRepository import ProductRepository
 from sqlalchemy.orm import Session
 from fastapi import Depends
 
+from app.services.userService import UserService
+
 
 def get_product_service(db: Session = Depends(get_db)) -> ProductService:
     product_repo = ProductRepository(db)
     return ProductService(product_repo)
+
+
+def get_user_service(db: Session = Depends(get_db)) -> UserService:
+    user_repo = UserRepository(db)
+
+    return UserService(user_repo)
