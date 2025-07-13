4b4c1bd3af6465f1448d8756cbf5a78bf7d634f1
Nanndo <gofernando44@gmail.com>
2025-07-13 16:18:01 -0300

    feat: import exception manipulators to useless and existing users

--- app/handlers/register.py
+++ app/handlers/register.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from app.exceptions.product import ProductNotFound, ProductNotUpdated
+from app.exceptions.users import UserAlreadyExists, UserNotFound
 from app.handlers.exception_handlers import (
     product_not_found_handler,
     http_exception_handler,
     product_not_updated_handler,
+    user_already_exists_handler,
+    user_not_found_handler,
 )
 
 
@@ -12,3 +15,5 @@ def register_exception_handlers(app: FastAPI):
     app.add_exception_handler(ProductNotFound, product_not_found_handler)
     app.add_exception_handler(HTTPException, http_exception_handler)
     app.add_exception_handler(ProductNotUpdated, product_not_updated_handler)
+    app.add_exception_handler(UserNotFound, user_not_found_handler)
+    app.add_exception_handler(UserAlreadyExists,user_already_exists_handler)
