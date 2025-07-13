494f74305d088b5a45546dc03c9e9b95ed5c605f
Nanndo <gofernando44@gmail.com>
2025-07-13 01:23:10 -0300

    feat: add update and delete methods to ProductService

--- app/services/productsService.py
+++ app/services/productsService.py
@@ -1,8 +1,8 @@
 import uuid
 from fastapi import HTTPException, status
-from app.exceptions.product import ProductNotFound
+from app.exceptions.product import ProductNotFound, ProductNotUpdated
 from app.repositories.productRepository import ProductRepository
-from app.schemas.productSchema import ProductCreate, ProductRead
+from app.schemas.productSchema import ProductCreate, ProductRead, ProductUpdate
 
 
 class ProductService:
@@ -12,9 +12,9 @@ class ProductService:
     def create_product_service(self, product: ProductCreate) -> ProductRead:
 
         product_exists = self.product_repo.get_by_sku(product.sku)
-        
+
         if product_exists:
-            
+
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail=f"Product with Sku '{product.sku}'  already exists.",
@@ -31,10 +31,40 @@ class ProductService:
         return [ProductRead.model_validate(product) for product in db_product_list]
 
     def get_by_id(self, product_id: uuid.UUID) -> ProductRead:
-        
+
         db_product_by_id = self.product_repo.get_by_id(product_id)
-        
+
         if not db_product_by_id:
             raise ProductNotFound(product_id=product_id)
-        
+
         return ProductRead.model_validate(db_product_by_id)
+
+    def update_product(
+        self, product: ProductUpdate, product_id: uuid.UUID
+    ) -> ProductRead:
+
+        db_product_by_id = self.product_repo.get_by_id(product_id)
+
+        if not db_product_by_id:
+            raise ProductNotFound(product_id=product_id)
+
+        update_data = product.model_dump(exclude_unset=True)
+        if not update_data:
+            raise ProductNotUpdated(
+                product_id=product_id, reason="No fields provided for update"
+            )
+
+        product_update = self.product_repo.update_product(product, product_id)
+
+        self.product_repo.db.commit()
+
+        return ProductRead.model_validate(product_update)
+
+    def delete_product(self, product_id: uuid.UUID) -> None:
+        db_product_by_id = self.product_repo.get_by_id(product_id)
+
+        if not db_product_by_id:
+            raise ProductNotFound(product_id=product_id)
+
+        self.product_repo.delete_product(product_id)
+        self.product_repo.db.commit()
