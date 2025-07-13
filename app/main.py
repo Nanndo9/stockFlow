d006bc3f64e1e8d7ecb7961955f4f06aa90da56e
Nanndo <gofernando44@gmail.com>
2025-07-13 16:21:49 -0300

    feat:Adds the users route to the application

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.core.dataBase import Base, engine
 from app.routes.productRoute import router as product_router
 from app.handlers.register import register_exception_handlers
-
+from app.routes.userRoute import router as user_router
 
 app = FastAPI(
     title="Stock Flow API", description="API para controle de estoque", version="1.0.0"
@@ -14,7 +14,7 @@ register_exception_handlers(app)
 Base.metadata.create_all(bind=engine)
 
 app.include_router(product_router)
-
+app.include_router(user_router)
 
 @app.get("/")
 def read_root():
