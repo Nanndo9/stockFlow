de928595d845b4deb0675edc72b859217482bf0a
Nanndo <gofernando44@gmail.com>
2025-07-13 01:16:34 -0300

    git add .
    feat:Add ProductNotFound and ProductNotUpdated custom exceptions

--- app/exceptions/product.py
+++ app/exceptions/product.py
@@ -1,9 +1,18 @@
-
 from sqlalchemy import UUID
 
 
 class ProductNotFound(Exception):
     def __init__(self, product_id: UUID):
         self.product_id = product_id
-        
+
         super().__init__(f"Product not found: {product_id}")
+
+
+class ProductNotUpdated(Exception):
+    def __init__(self, product_id: UUID, reason: str = "unknown reason"):
+        self.product_id = product_id
+        self.reason = reason
+
+        super().__init__(
+            f"Product with ID '{product_id}' could not be updated: {reason}"
+        )
